abbccc47efe75aa96059db9a44d6763c5d1bded0
rudyhuacasi <alexisitaliano16@gmail.com>
2025-04-28 18:26:01 +0200

    {UPD} added cardpokemory name and pokemon card

--- src/components/CardPokemory.jsx
+++ src/components/CardPokemory.jsx
@@ -5,10 +5,11 @@ function CardPokemory({ card, onClick, isFlipped }) {
 
   return (
     <>
-      <div className="memory-card" onClick={() => onClick(card)}>
+      <div className="memory-card col-3 rounded-3" onClick={() => onClick(card)}>
       <div className={`inner ${isFlipped ? 'flipped' : ''}`}>
-        <div className="front">
+        <div className="front d-flex flex-column">
           <img src={card.image} alt={card.name} />
+          <h3 className='mayus'>{card.name}</h3>
         </div>
       </div>
     </div>
